4359765ea43f942f7536c572ab181f1485a16dfa
Paul DUTOIT <pdutoit@datasolution.fr>
2021-08-19 21:37:47 +0200

    customize menu

--- web/app/themes/montheme/functions.php
+++ web/app/themes/montheme/functions.php
@@ -9,6 +9,23 @@ require_once (get_template_directory() . '/core/theme_setup.php');
 require_once (get_template_directory() . '/settings/theme_settings.php');
 require_once (get_template_directory() . '/settings/settings_getters.php');
 
+
+/**
+ * Theme Customizer
+ */
+
+require_once( get_template_directory() . "/customizer/theme_customizer.php" );
+/* Setup the Theme Customizer settings and controls*/
+add_action( 'customize_register', array( 'HERITAGE_Customize', 'register' ) );
+add_action( 'customize_controls_enqueue_scripts', array( 'HERITAGE_Customize', 'register_controls' ) );
+
+/* Output customizer CSS to live site - customizer colors*/
+add_action( 'wp_head', array( 'HERITAGE_Customize', 'header_output' ) );
+
+/* Enqueue live preview javascript in Theme Customizer admin screen*/
+add_action( 'customize_preview_init', array( 'HERITAGE_Customize', 'live_preview' ) );
+
+
 /**
  * Load needed  js scripts and css styles
  */
@@ -38,6 +55,12 @@ require_once(get_template_directory() . "/core/utils.php");
 */
 
 require_once( get_template_directory() . "/core/comments_template_cbk.php" );
+
+
+/*
+      wp_ajax actions
+  */
+require_once( get_template_directory() . "/core/ajax_binding.php" );
 
 /**
  * Menu walker
